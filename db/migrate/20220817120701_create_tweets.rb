790450fe5710987800c1bc6a463003ff622dbb42
saqlainabbas512 <saqlainkhichi159@gmail.com>
2022-08-17 20:15:54 +0500

    models and migrations and associations built

---
+++ db/migrate/20220817120701_create_tweets.rb
@@ -0,0 +1,10 @@
+class CreateTweets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tweets do |t|
+      t.integer :user_id
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/saqlain_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SaqlainTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
